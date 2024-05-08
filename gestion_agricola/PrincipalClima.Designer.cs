25e3123040c233ca88e0fb55054b880b144182fe
Enrique Medina <enriquemedina880@gmail.com>
2024-05-08 00:54:31 -0600

    Especificaciones Funciones que tendra analisis clima

--- gestion_agricola/PrincipalClima.Designer.cs
+++ gestion_agricola/PrincipalClima.Designer.cs
@@ -28,18 +28,73 @@
         /// </summary>
         private void InitializeComponent()
         {
+            lbBienvenida = new Label();
+            btnAH = new Button();
+            btnAM = new Button();
+            btnInforme = new Button();
             SuspendLayout();
+            // 
+            // lbBienvenida
+            // 
+            lbBienvenida.AutoSize = true;
+            lbBienvenida.Font = new Font("Yu Gothic", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
+            lbBienvenida.Location = new Point(163, 9);
+            lbBienvenida.Name = "lbBienvenida";
+            lbBienvenida.Size = new Size(241, 27);
+            lbBienvenida.TabIndex = 0;
+            lbBienvenida.Text = "ANALISIS DEL CLIMA ";
+            lbBienvenida.Click += lbBienvenida_Click;
+            // 
+            // btnAH
+            // 
+            btnAH.Font = new Font("Yu Gothic", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            btnAH.Location = new Point(12, 83);
+            btnAH.Name = "btnAH";
+            btnAH.Size = new Size(138, 37);
+            btnAH.TabIndex = 1;
+            btnAH.Text = "Analisis Hoy";
+            btnAH.UseVisualStyleBackColor = true;
+            // 
+            // btnAM
+            // 
+            btnAM.Font = new Font("Yu Gothic", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            btnAM.Location = new Point(178, 83);
+            btnAM.Name = "btnAM";
+            btnAM.Size = new Size(226, 37);
+            btnAM.TabIndex = 2;
+            btnAM.Text = "Analisis Periodo 15-30 dias";
+            btnAM.UseVisualStyleBackColor = true;
+            // 
+            // btnInforme
+            // 
+            btnInforme.Font = new Font("Yu Gothic", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            btnInforme.Location = new Point(410, 83);
+            btnInforme.Name = "btnInforme";
+            btnInforme.Size = new Size(156, 37);
+            btnInforme.TabIndex = 3;
+            btnInforme.Text = "Informe Analisis";
+            btnInforme.UseVisualStyleBackColor = true;
             // 
             // PrincipalClima
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(578, 309);
+            Controls.Add(btnInforme);
+            Controls.Add(btnAM);
+            Controls.Add(btnAH);
+            Controls.Add(lbBienvenida);
             Name = "PrincipalClima";
             Text = "PrincipalClima";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label lbBienvenida;
+        private Button btnAH;
+        private Button btnAM;
+        private Button btnInforme;
     }
 }
--- gestion_agricola/PrincipalClima.cs
+++ gestion_agricola/PrincipalClima.cs
@@ -16,5 +16,10 @@ namespace gestion_agricola
         {
             InitializeComponent();
         }
+
+        private void lbBienvenida_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
