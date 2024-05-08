77ac04f6f2b820bcacb6d238e180facdbbb73980
moises martinez <moisesalejandromar8@gmail.com>
2024-05-08 10:10:56 -0600

    he arreglado el error del acceso denegado

---
+++ gestion_agricola/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gestion_agricola
+{
+    public class Class1
+    {
+        int agua;
+        string pereza;
+    }
+}
